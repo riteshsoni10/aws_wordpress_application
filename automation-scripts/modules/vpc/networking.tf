ba16612f3e646c93e8bd0046eff332b601c19560
riteshsoni10 <riteshk.296@gmail.com>
2020-09-03 05:23:11 +0530

    Added route table association

--- automation-scripts/modules/vpc/networking.tf
+++ automation-scripts/modules/vpc/networking.tf
@@ -26,3 +26,14 @@ resource "aws_route_table" "public_route_table" {
         aws_vpc.web_server_vpc,
     ]
 }
+
+
+resource "aws_route_table_association" "public_route_table_association" {
+    count          = "${length(data.aws_availability_zones.available.names)}" 
+    subnet_id      = "${element(aws_subnet.public_subnet.*.id,count.index)}"
+    route_table_id = aws_route_table.public_route_table.id
+
+    depends_on = [
+        aws_route_table.public_route_table
+    ]
+}

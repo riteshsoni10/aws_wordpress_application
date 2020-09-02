e49a5ddf8177149bcb13cc06b377b68e4e2bf975
riteshsoni10 <riteshk.296@gmail.com>
2020-09-03 05:23:43 +0530

    Added private subnets

--- automation-scripts/modules/vpc/vpc.tf
+++ automation-scripts/modules/vpc/vpc.tf
@@ -23,15 +23,31 @@ locals {
 
 
 resource "aws_subnet" "public_subnet"{
-    count = "${length(data.aws_availability_zones.available.names)}"
-    vpc_id = aws_vpc.web_server_vpc.id
-    cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, count.index)}"
-    availability_zones = "${element(data.aws_availability_zones.available.names,count.index)}"
+    count                   = "${length(data.aws_availability_zones.available.names)}"
+    vpc_id                  = aws_vpc.web_server_vpc.id
+    cidr_block              = "${cidrsubnet(var.vpc_cidr_block, 8, count.index)}"
+    availability_zone       = "${element(data.aws_availability_zones.available.names,count.index)}"
     map_public_ip_on_launch = true
 
-    tags        = {
-            Name = "Public-${element(data.aws_availability_zones.available.names,count.index)}"
+    tags                    = {
+                Name = "Public Subnet -${element(data.aws_availability_zones.available.names,count.index)}"
     }
+    depends_on =[
+        aws_vpc.web_server_vpc,
+    ]
+}
+
+resource "aws_subnet" "private_subnet"{
+    count                   = "${length(data.aws_availability_zones.available.names)}"
+    vpc_id                  = aws_vpc.web_server_vpc.id
+    cidr_block              = "${cidrsubnet(var.vpc_cidr_block, 8, 10+count.index)}"
+    availability_zone       = "${element(data.aws_availability_zones.available.names,count.index)}"
+    map_public_ip_on_launch = false
+
+    tags                    = {
+                Name = "Private Subnet - ${element(data.aws_availability_zones.available.names,count.index)}"
+    }
+
     depends_on =[
         aws_vpc.web_server_vpc,
     ]

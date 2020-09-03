95f75cf45ebe7fd63210d38dbdfb02a2a803369b
riteshsoni10 <riteshk.296@gmail.com>
2020-09-03 05:55:22 +0530

    added module

--- automation-scripts/main.tf
+++ automation-scripts/main.tf
@@ -3,10 +3,18 @@ provider "aws"{
     region  = var.region_name
 }
 
-module "web_server" {
-    source              = "./modules/web_server"
+module "vpc" {
+    source              = "./modules/vpc"
     vpc_cidr_block      = var.vpc_cidr_block
     dns_support_state   = var.vpc_dns_support_state
     dns_hostnames_state = var.vpc_dns_hostnames_state
 
 }
+
+module "web_server" {
+    source = "./modules/web_server"
+    ami_id  = var.instance_ami
+    vpc_id = module.vpc.vpc_id
+    public_subnet_id = module.vpc.public_subnets.0
+}
+

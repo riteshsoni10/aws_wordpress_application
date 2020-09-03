c54a1ade47865e4fe781be8519e96018bbd5b236
riteshsoni10 <riteshk.296@gmail.com>
2020-09-03 06:30:10 +0530

    added db server

---
+++ automation-scripts/modules/database_server/ec2.tf
@@ -0,0 +1,17 @@
+## Web Server Instance
+resource "aws_instance" "db_server" {
+    ami                         = var.ami_id
+    instance_type               = var.instance_type
+    subnet_id                   = var.private_subnet_id
+    vpc_security_group_ids      = [aws_security_group.db_security_group.id]
+    key_name                    = aws_key_pair.db_key_pair.key_name
+    tags                        = {
+                        Name = "Database-Server"
+    }
+
+    depends_on  = [
+        aws_security_group.db_security_group,
+        aws_key_pair.db_key_pair,
+    ]
+}
+
